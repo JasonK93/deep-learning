70e714a7ae4b8874fea73f574649fcc6b2250600
JasonK93 <xueli81993@gmail.com>
2017-11-18 13:46:04 -0500

    Update 2.MLP.py
    update

--- 2.MLP.py
+++ 2.MLP.py
@@ -1,18 +1,3 @@
-# %%
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
 import tensorflow as tf
 import logging
 
@@ -42,7 +27,7 @@ hidden1 = tf.nn.relu(tf.matmul(x, W1) + b1)
 hidden1_drop = tf.nn.dropout(hidden1, keep_prob)
 
 y = tf.nn.softmax(tf.matmul(hidden1_drop, W2)+ b2)
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_*tf.log(y), reduction_indices=[1]))
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_))
 
 train_step = tf.train.AdagradOptimizer(0.3).minimize(cross_entropy)
 
